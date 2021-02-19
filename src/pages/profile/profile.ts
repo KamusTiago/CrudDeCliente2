fe79d02b112f8de878655d4d853693b755c724b0
KamusTiago <tiagokamus40@gmail.com>
2021-02-19 17:42:00 -0300

    buscando endereco no banco de dados

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,7 +26,7 @@ export class ProfilePage {
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
-          this.cliente = response;
+          this.cliente = response as ClienteDTO; //a resposta vai casar com os dados do cliente
           this.getImageIfExists();
         },
           error => {
