a80c0137f9c6806dc53d08aa7cbd9fd2665d44ed
KamusTiago <tiagokamus40@gmail.com>
2021-02-19 20:46:47 -0300

    informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -54,11 +55,15 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  voltarParaHome(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido)
     .subscribe(response =>{
       this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codpedido = this.extraiaId(response.headers.get('location'));
     },
     error =>{
       if(error.status == 403){
@@ -67,4 +72,8 @@ export class OrderConfirmationPage {
     });
   }
 
+  private extraiaId(location: string) : string{
+    let posicao = location.lastIndexOf('/');
+    return location.substring(posicao + 1, location.length);
+  }
 }
