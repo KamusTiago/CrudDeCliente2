22fa13f06ccbdb0010556f71c0e103e6264e9c46
KamusTiago <tiagokamus40@gmail.com>
2021-02-20 14:42:59 -0300

    atualizacao de imagem de perfil no dispositivo movel

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
@@ -16,20 +17,24 @@ export class ProfilePage {
   cliente: ClienteDTO;
   picture: string;
   cameraOn: boolean = false;
+  profileImage;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera) {
+    public camera: Camera,
+    public sanitizer: DomSanitizer) {
+
+    this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
     this.carregarDados();
   }
 
-  carregarDados(){
+  carregarDados() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -42,7 +47,7 @@ export class ProfilePage {
               this.navCtrl.setRoot('HomePage');
             }
           });
-    } 
+    }
     else {
       this.navCtrl.setRoot('HomePage');
     }
@@ -52,31 +57,47 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
       .subscribe(response => {
         this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+        this.blobToDataURL(response).then(dataUrl => {
+          let str: string = dataUrl as string;
+          this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+        });
       },
-        error => { });
+        error => {
+          this.profileImage = 'assets/imgs/avatar-blank.png';
+        });
   }
 
-  getCameraPicture(){
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    })
+  }
+
+  getCameraPicture() {
     this.cameraOn = true;
-    
+
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
-    
+
     this.camera.getPicture(options).then((imageData) => {
-     this.picture = 'data:image/png;base64,' + imageData;
-     this.cameraOn = false;
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
     }, (err) => {
       this.cameraOn = false;
     });
   }
 
-  getGaleryPicture(){
+  getGaleryPicture() {
     this.cameraOn = true;
-    
+
     const options: CameraOptions = {
       quality: 100,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
@@ -84,28 +105,28 @@ export class ProfilePage {
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
-    
+
     this.camera.getPicture(options).then((imageData) => {
-     this.picture = 'data:image/png;base64,' + imageData;
-     this.cameraOn = false;
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
     }, (err) => {
       this.cameraOn = false;
     });
   }
 
-  sendPicture(){
+  sendPicture() {
     this.clienteService.uploadPicture(this.picture)
-    .subscribe(response =>{
-      this.picture = null;
-      this.carregarDados();
-    },
-    error =>{
+      .subscribe(response => {
+        this.picture = null;
+        this.getImageIfExists();
+      },
+        error => {
 
-    }
-    )
+        }
+      )
   }
 
-  cancel(){
+  cancel() {
     this.picture = null;
   }
 }
